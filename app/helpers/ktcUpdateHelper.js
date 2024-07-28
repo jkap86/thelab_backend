0de72a3479ed0428dc05d279d95da674181978df
Jay Kapoor <jkap86@gmail.com>
2024-07-28 09:19:03 -0400

    -

--- app/helpers/ktcUpdateHelper.js
+++ app/helpers/ktcUpdateHelper.js
@@ -31,10 +31,7 @@ const matchKtcToSleeperId = (player_value_obj) => {
 
 const getDailyKtcValues = async () => {
   console.log("Getting Daily KTC values...");
-  const browser = await puppeteer.launch({
-    executablePath: puppeteer.executablePath(),
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
 
